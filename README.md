b347551dbbd4277baeccbca2507205851efa513d
Leonor <ldominoni@gmail.com>
2023-02-21 15:11:13 -0300

    index

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# buscando-CEP
- Projeto utilizando API
+# js-consumindo-dados-api
+Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API" da Alura Cursos Online.
---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Oi pro migo')
+function mandaMensagem(){
+    console.log('oi jovem')
+    console.log('To em casa')
+    console.log('ta bom')
+}
+
+setTimeOut(mandaMensagem, 3000)
+
+console.log('acabou')
