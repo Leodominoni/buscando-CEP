c22014dc804d48947e272da6f738ebd87a8b2247
Leonor <ldominoni@gmail.com>
2023-02-22 16:40:03 -0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-console.log('Oi pro migo')
-function mandaMensagem(){
-    console.log('oi jovem')
-    console.log('To em casa')
-    console.log('ta bom')
+async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ''
+    try{
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    var consultaCepConvertida = await consultaCep.json()
+    if (consultaCepConvertida.erro) {
+        throw Error('CEP não existe')
+    }
+    var cidade = document.getElementById('cidade')
+    var logradouro = document.getElementById('endereco')
+    var estado = document.getElementById('estado')
+    var bairro = document.getElementById('bairro')
+
+    cidade.value = consultaCepConvertida.logradouro
+    logradouro.value = consultaCepConvertida.logradouro
+    estado.value = consultaCepConvertida.uf
+    bairro.value = consultaCepConvertida.bairro
+    
+    console.log(consultaCepConvertida)
+    return consultaCepConvertida
+}   catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido</p>`
+    console.log(erro)
 }
-
-setTimeOut(mandaMensagem, 3000)
-
-console.log('acabou')
+}
+var cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
